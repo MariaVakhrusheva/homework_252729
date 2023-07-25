5394c85da5a0bebd4c55084ab9d87d9d801546d3
Marya <2510marycat@gmail.com>
2023-07-25 10:56:43 +0500

    Задача №25, №27, №29

---
+++ Task_27/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+int GetSumOfDigits(int number)
+{
+    int sum = 0;
+    int temp = number;
+    while (number > 0)
+    {
+        sum = sum + number % 10;
+        number /= 10;
+    }
+    Console.WriteLine($"{temp} -> {sum}");
+    return sum;
+}
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int number = GetInput("Введите число: ");
+GetSumOfDigits(number);
---
+++ Task_29/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+void ShowCubes(int number)
+{
+    Console.Write($"{number} -> ");
+    for (int i = 1; i < number; i++)
+    {
+        Console.Write($"{i * i * i}, ");
+    }
+    Console.Write(number * number * number);
+}
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = GetInput("Введите число: ");
+ShowCubes(number);
